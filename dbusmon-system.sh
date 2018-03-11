c66df1a5ee451db34b754fc904ca7e95c350004d
Roger Wilco <37160382+user-resu@users.noreply.github.com>
2018-03-11 01:18:03 +0100

    poc - well...logic stays, it does it's job

--- dbusmon-system.sh
+++ dbusmon-system.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 STATE_LOCK=0
+STATE_BATT=0
 
 dbus-monitor --system | 
-    while read -r MESSAGE; do
-        #echo "$MESSAGE"
-        echo ">>>$([[ "$MESSAGE" =~ ^.*?\"(LockedHint)\"$ ]] && echo "${BASH_REMATCH[0]}")"
+    while read -r MESSAGE
+    do
         if [[ $MESSAGE =~ ^.*\"LockedHint\"$ ]]
         then
-            echo "Locked state changed"
+            echo "Desktop is"
             STATE_LOCK=1
         elif [[ $STATE_LOCK -eq 1 ]]
         then
@@ -16,15 +16,33 @@ dbus-monitor --system |
             then
                 case "${BASH_REMATCH[1]}" in
                     true)
-                        echo "to locked"
+                        echo "  locked"
                         STATE_LOCK=0
                     ;;
                     false)
-                        echo "to unlocked"
+                        echo "  unlocked"
                         STATE_LOCK=0
                     ;;
                 esac
             fi
+        elif [[ "$MESSAGE" =~ ^.*?\"OnBattery\"$ ]]
+        then
+            echo "Power is"
+            STATE_BATT=1
+        elif [[ $STATE_BATT -eq 1 ]]
+        then
+            if [[ $MESSAGE =~ ^.*?boolean[[:space:]](.*?)$ ]]
+            then
+                case "${BASH_REMATCH[1]}" in
+                    true)
+                        echo "  disconneccted"
+                        STATE_BATT=0
+                    ;;
+                    false)
+                        echo "  connected"
+                        STATE_BATT=0
+                    ;;
+                esac
+            fi
         fi
-
     done
